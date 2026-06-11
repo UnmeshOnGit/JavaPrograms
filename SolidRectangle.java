ec4b3647ca84fa4938d9ba84389bd785de3bd01a
Unmesh Sutar <108141720+UnmeshOnGit@users.noreply.github.com>
2026-06-11 12:33:03 +0530

    Add SolidRectangle class to print asterisks
    Implement a program to print a solid rectangle of asterisks.

---
+++ SolidRectangle.java
@@ -0,0 +1,22 @@
+// Problem
+// *****
+// *****
+// *****
+// *****
+// *****
+
+import java.util.*;
+public class SolidRectangle{
+    public static void main(String[] args){
+        int n = 5;
+        int m = 5;
+
+        for(int i=1; i<=5; i++){
+            for(int j=1;j<=5; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
