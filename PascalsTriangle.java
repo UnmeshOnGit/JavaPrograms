5420d6738aef80dbff9989ddfd3d1283ba8858ae
Unmesh Sutar <108141720+UnmeshOnGit@users.noreply.github.com>
2026-06-16 13:47:02 +0530

    Create PascalsTriangle.java

---
+++ PascalsTriangle.java
@@ -0,0 +1,27 @@
+// Print Pascal’s Triangle.
+
+//     1
+//    1 1
+//   1 2 1
+//  1 3 3 1
+// 1 4 6 4 1
+
+
+class PascalsTriangle{
+    public static void main(String[] args) {
+        int n=5;
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n-i; j++){
+                System.out.print(" ");
+            }
+            for(int j=1; j<=i; j++){
+                if(j==1 || j==i){
+                    System.out.print("1 ");
+                }else{
+                    System.out.print((i-1) + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
