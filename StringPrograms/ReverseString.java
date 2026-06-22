7ef31dcd51d06f01e36172c1b42dc38e023b8d86
Unmesh Sutar <108141720+UnmeshOnGit@users.noreply.github.com>
2026-06-22 19:29:58 +0530

    Create ReverseString.java

---
+++ StringPrograms/ReverseString.java
@@ -0,0 +1,22 @@
+//reverse string using stringBuilder class
+
+import java.util.*;
+class ReverseString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in); 
+        StringBuilder sb = new StringBuilder("Harsh");
+
+        for(int i=0; i<sb.length()/2; i++){
+            int front = i;
+            int back = sb.length() - i - 1;
+
+            char frontChar = sb.charAt(front);
+            char backChar = sb.charAt(back);
+
+            sb.setCharAt(front , backChar);
+            sb.setCharAt(back , frontChar);
+        }
+        System.out.println(sb);
+
+    }
+}
